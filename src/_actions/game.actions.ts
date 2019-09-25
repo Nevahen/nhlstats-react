ef72f404fae9c6ed451c35b64fa1902bd3003649
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2019-09-25 21:39:26 +0300

    less magic in the world

--- src/_actions/game.actions.ts
+++ src/_actions/game.actions.ts
@@ -1,6 +1,8 @@
 import { GameEventTypes, IGameEvent } from '../_types/GameEvent';
 import { GameState } from '../_types/GameState';
 
+const REGULAR_LAST_PERIOD = 3;
+
 export const addPlayer = (player: { name: string, id: number }) => {
   return {
     type: 'ADD_PLAYER',
@@ -12,15 +14,17 @@ export const gameEvent = (event: IGameEvent) => {
   return (dispatch: any, getState: any) => {
     const gameState = getState().newgame as GameState;
 
-    if(event.type === GameEventTypes.PERIOD && gameState.period < 3) {
+    if(event.type === GameEventTypes.PERIOD && gameState.period < REGULAR_LAST_PERIOD) {
       dispatch({
         type: 'NEW_PERIOD',
       })
     }
+
+    // TODO. If overtime and goal, end game.
 
     dispatch ({
       type: 'GAME_EVENT',
       event
     });
   }
-} 
+}
