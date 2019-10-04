7a800b20a2f040152f1b6e1f42331f698b100b86
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2019-10-04 19:47:25 +0300

    reexports

--- src/_types/index.ts
+++ src/_types/index.ts
@@ -1,5 +1,13 @@
 import { GameStatus } from './GameStatus';
 
+export * from './GameEvent';
+export * from './AuthenticationState';
+export * from './GameState';
+export * from './ITeam';
+export * from './MatchStore';
+export * from './TeamStore';
+export * from './UserStore';
+
 export {
   GameStatus,
 }
